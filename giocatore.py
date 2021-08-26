c2815c181a738f1d8024833d13ce00631d1fa3d7
David92p <dave.panetta92@gmail.com>
2021-08-26 15:54:40 +0200

    Fix sulla creazione di un giocatore

--- giocatore.py
+++ giocatore.py
@@ -9,41 +9,34 @@ class Giocatore:
     #Il seguente metodo prepara un giocatore ad una partita di briscola 
     @staticmethod
     def inizioGioco():
-        carte = []
+        giocatori = {}
+        giocatori[input('Inserisci il nome del giocatore: ')] = []
         deck = Deck()
-        for carta in range(3):
-            cartaPescata = Deck.consegnaCarta(deck)
-            carte.append(cartaPescata)
-        return carte
+        for giocatore, carte in giocatori.items():
+            for i in range(3):
+                cartaPescata = Deck.consegnaCarta(deck)
+                giocatori[giocatore].append(cartaPescata)
+        return giocatori
             
-            
+    #Il seguente metodo ci permette di visualizzare a schermo attraverso un print il nome del giocatore istanziato con le rispettive carte        
     def stampaCarte(self):
-        cartePescate = ""
-        for carta in self.carteGiocatore:
-            cartePescate +=  carta +"  "
-        print("*"*70)
-        print("|"+" "*68+"|")
-        print("|", end="")
-        print(cartePescate.center(68, " "), end="")
-        print("|")
-        print("|"+" "*68+"|")
-        print("*"*70)
+        for nomeGiocatore, carte in self.carteGiocatore.items():
+            print('Giocatore '+ nomeGiocatore + ':')
+            print()
+            print(carte)
+            print()
+        
 
 
 
 #test
 if __name__ == "__main__":
-    deck = Deck()
-    giocatore1 = Giocatore()
-    giocatore2 = Giocatore()
+    deck = Deck() # Istanziamo un oggetto di tipo Deck
+    giocatore1 = Giocatore() # Istanziamo un primo giocatore 
+    Giocatore.stampaCarte(giocatore1) # Richiamiamo un metodo di stampa che l'avora sull'oggetto creato
     print()
-    Deck.mostraDeck(deck)
-    print()
-    Giocatore.stampaCarte(giocatore1)
-    print()
-    Giocatore.stampaCarte(giocatore2)
-    print()
-    Deck.mostraDeck(deck)
+    Deck.mostraDeck(deck) # stampiamo il mazzo di carte rimanente come debugging
+
 
     
 
