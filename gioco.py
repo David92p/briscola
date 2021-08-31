21d0061ccb7b4d11f36e4f80c88c8d0b48fe4881
David92p <dave.panetta92@gmail.com>
2021-08-31 16:48:57 +0200

    Creata Classe di gioco iniziale

---
+++ gioco.py
@@ -0,0 +1,37 @@
+from deck import Deck
+from giocatore import Giocatore
+
+from random import shuffle, randrange
+
+# La classe ci occupa di creare un oggetto di gioco di tipo Briscola con una lista nel seguente ordine: Deck - Giocatore 1 - Giocatore 2 
+class Briscola:
+    def __init__(self):
+        self.briscola = Briscola.inizioGioco()
+
+    # Il metodo ci aiuta a completare la nostra lista con i dati richiesti nell'ordine sopra descritto, inoltre ci da la possibilità di 
+    # inserire i nomi dei giocatori partecipanti al gioco
+    @staticmethod
+    def inizioGioco():
+        briscola = []
+        briscola.append(Deck())
+        for giocatore in range(2):
+            print('Inserire il nome del giocatore: ')
+            briscola.append(Giocatore())
+        return briscola
+
+    # test
+    def giochiamo(self):
+        Deck.mostraDeck(self.briscola[0])
+        print()
+        Giocatore.stampaCarte(self.briscola[1])
+        print()
+        Giocatore.stampaCarte(self.briscola[2])
+    
+
+# test
+briscola = Briscola()
+Briscola.giochiamo(briscola)
+
+
+
+
