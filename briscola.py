4757dedf324467435c12424a154a5e2348ee0b6b
David92p <davide.panetta@hotmail.com>
2021-12-27 07:06:35 +0100

    fix briscola functions

--- briscola.py
+++ briscola.py
@@ -1,9 +1,9 @@
 from deck import Deck
 from giocatore import Giocatore
+from printer import Printer
 
 from random import randint
 
-from printer import Printer
 
 # La classe ci occupa di creare un oggetto di gioco di tipo Briscola con una lista nel seguente ordine: Deck - Giocatore 1 - Giocatore 2
 
@@ -31,69 +31,96 @@ class Briscola:
 
         return briscola
 
-    
+    def definisciPresa(self, prima_carta, seconda_carta):
+        conteggioDeck = [["Asso Coppe", "3 di Coppe", "Re di Coppe", "Cavallo di Coppe", "Donna di Coppe", "7 di Coppe",
+                          "6 di Coppe", "5 di Coppe", "4 di Coppe", "2 di Coppe"],
+                         ["Asso Spade", "3 di Spade", "Re di Spade", "Cavallo di Spade", "Donna di Spade", "7 di Spade",
+                          "6 di Spade", "5 di Spade", "4 di Spade", "2 di Spade"],
+                         ["Asso Bastoni", "3 di Bastoni", "Re di Bastoni", "Cavallo di Bastoni", "Donna di Bastoni", "7 di Bastoni",
+                          "6 di Bastoni", "5 di Bastoni", "4 di SBastoni", "2 di Bastoni"],
+                         ["Asso Denari", "3 di Denari", "Re di Denari", "Cavallo di Denari", "Donna di Denari", "7 di Denari",
+                          "6 di Denari", "5 di Denari", "4 di Denari", "2 di Denari"]]
 
-    def inizio(self):
-        random_start = randint(0, 1)
-        print(
-            f"INIZIERA' IL GIOCATORE {Giocatore.stampaNome(self.giocoBriscola[2][random_start])}")
-
-        conteggio = [self.giocoBriscola[1]]
-        giocatore_1 = []
-        giocatore_2 = []
-
-        while True:
-            try:
-                first_start = input(
-                    f"{Giocatore.stampaNome(self.giocoBriscola[2][random_start])} PREMI INVIO SE SEI PRONTO A GIOCARE!!")
-                print()
-                if first_start == "":
-                    Giocatore.stampaCarte(self.giocoBriscola[2][random_start])
-                    print()
-                    print("LA BRISCOLA DI GIOCO SARA'")
-                    Printer.stampaCarta(self.giocoBriscola[1])
-                    break
-                else:
-                    print(
-                        f'VALORE NON RICONOSCIUTO - {Giocatore.stampaNome(self.giocoBriscola[2][random_start])} PREMI INVIO SE SEI PRONTO A GIOCARE')
-            except ValueError:
-                print(
-                    f'VALORE NON RICONOSCIUTO - {Giocatore.stampaNome(self.giocoBriscola[2][random_start])} PREMI INVIO SE SEI PRONTO A GIOCARE')
-
-    
-            
-
-    def test(self,prima_carta, seconda_carta):
-        conteggioDeck = [["Asso Coppe", "3 di Coppe", "Re di Coppe", "Donna di Coppe", "Cavallo di Coppe", "7 di Coppe",
-                            "6 di Coppe", "5 di Coppe", "4 di Coppe", "2 di Coppe"],
-                        ["Asso Spade", "3 di Spade", "Re di Spade", "Donna di Spade", "Cavallo di Spade", "7 di Spade",
-                            "6 di Spade", "5 di Spade", "4 di Spade", "2 di Spade"],
-                        ["Asso Bastoni", "3 di Bastoni", "Re di Bastoni", "Donna di Bastoni", "Cavallo di Bastoni", "7 di Bastoni",
-                            "6 di Bastoni", "5 di Bastoni", "4 di SBastoni", "2 di Bastoni"],
-                        ["Asso Denari", "3 di Denari", "Re di Denari", "Donna di Denari", "Cavallo di Denari", "7 di Denari",
-                            "6 di Denari", "5 di Denari", "4 di Denari", "2 di Denari"]]
- 
-        if self.giocoBriscola[1][0] != prima_carta[0] and self.giocoBriscola[1][0] != seconda_carta[0]:
+        # condizione se nessuno dei due giocatori o entrambi giocano una carta briscola
+        if self.giocoBriscola[1][0] != prima_carta[0] and self.giocoBriscola[1][0] != seconda_carta[0] or self.giocoBriscola[1][0] == seconda_carta[0] and self.giocoBriscola[1][0] == prima_carta[0]:
+            # in caso di nessuna carta briscola da entrambe le parti vince il giocatore 1 che ha effettuato la prima giocata
             if prima_carta[0] != seconda_carta[0]:
-               return 0
-            elif prima_carta[0] == seconda_carta[0] or self.giocoBriscola[1][0] == seconda_carta[0] and self.giocoBriscola[1][0] == prima_carta[0]:
+                return 0
+            # in caso di carte diverse o entrambi briscole si contano i punti di validità della carta
+            elif prima_carta[0] == seconda_carta[0]:
                 listaCarte = conteggioDeck[prima_carta[0]]
                 index_prima_carta = listaCarte.index(prima_carta[1])
+                print(index_prima_carta)
                 index_seconda_carta = listaCarte.index(seconda_carta[1])
+                print(index_seconda_carta)
                 if index_prima_carta < index_seconda_carta:
-                        return 0
+                    return 0
                 else:
                     return 1
+        # condizione se giocatore 1 gioca una carta briscola
         elif self.giocoBriscola[1][0] == prima_carta[0] and self.giocoBriscola[1][0] != seconda_carta[0]:
             return 0
+        # condizione se giocatore 2 gioca una carta briscola
         elif self.giocoBriscola[1][0] == seconda_carta[0] and self.giocoBriscola[1][0] != prima_carta[0]:
             return 1
-        
+
+    def definisciGiocata(self):
+        carte = 0
+        carte_giocatore_1 = []
+        carte_giocatore_2 = []
+        while carte < 40:
+            turno = randint(0, 1)
+            giocata = 1
+            carte_giocate = []
+            while giocata < 3:
+                try:
+                    giocatore = input(
+                        f"{Giocatore.stampaNome(self.giocoBriscola[2][turno])} PREMI INVIO SE SEI PRONTO A GIOCARE!!")
+                    print()
+                    if giocatore == "":
+                        Giocatore.stampaCarte(
+                            self.giocoBriscola[2][turno])
+                        print()
+                        print("LA BRISCOLA DI GIOCO SARA'")
+                        Printer.stampaCarta(self.giocoBriscola[1])
+                        carta = Giocatore.giocaCarta(
+                            self.giocoBriscola[2][turno])
+                        carte_giocate.append(carta)
+                        if turno == 0:
+                            turno = 1
+                        else:
+                            turno = 0
+
+                        carta_consegnata = Deck.consegnaCarta(
+                            self.giocoBriscola[0])
+                        Giocatore.pescaCarta(
+                            self.giocoBriscola[2][turno], carta_consegnata)
+
+                        giocata += 1
+
+                    else:
+                        print(
+                            f'VALORE NON RICONOSCIUTO - {Giocatore.stampaNome(self.giocoBriscola[2][turno])} PREMI INVIO SE SEI PRONTO A GIOCARE')
+                except ValueError:
+                    print(
+                        f'VALORE NON RICONOSCIUTO - {Giocatore.stampaNome(self.giocoBriscola[2][turno])} PREMI INVIO SE SEI PRONTO A GIOCARE')
+
+                vincitore = Briscola.definisciPresa(
+                    self.giocoBriscola, carte_giocate[0], carte_giocate[1])
+                if vincitore == 0:
+                    carte_giocatore_1.append(carte_giocate[0])
+                    carte_giocatore_1.append(carte_giocate[1])
+                    turno = 0
+                if vincitore == 1:
+                    carte_giocatore_2.append(carte_giocate[0])
+                    carte_giocatore_2.append(carte_giocate[1])
+                    turno = 1
+
+                carte_giocate.clear()
+                giocata = 1
+                carte += 1
 
 
-# test
-# deck = Deck()
-briscola = Briscola()  # Istanziamo un oggetto Briscola composto da un dizionario con 3 valori chiave nel seguente ordine - deck, briscola, giocatori
-#Briscola.inizio(briscola)
-Briscola.inizio(briscola)
-print(Briscola.test(briscola, [1,"Cavallo di Spade"],[3, "Re di Denari"]))
+if __name__ == "__main__":
+    gioco = Briscola()
+    Briscola.definisciGiocata(gioco)
