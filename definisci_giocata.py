f0dd5513609b7b925cf214f63101052bcb74cded
David92p <dave.panetta92@gmail.com>
2021-12-28 17:00:29 +0100

    fix funzione definizione giocata

--- definisci_giocata.py
+++ definisci_giocata.py
@@ -11,12 +11,12 @@ class ValutazioneCarte:
                          ["Asso Denari", "3 di Denari", "Re di Denari", "Cavallo di Denari", "Donna di Denari", "7 di Denari",
                           "6 di Denari", "5 di Denari", "4 di Denari", "2 di Denari"]]
 
-        # condizione se nessuno dei due giocatori o entrambi giocano una carta briscola
-        if cartaBriscola[0] != carta1[0] and cartaBriscola[0] != carta2[0] or cartaBriscola[0] == carta2[0] and cartaBriscola[0] == carta1[0]:
+        # condizione se nessuno dei due giocatori giocano una carta briscola
+        if cartaBriscola[0] != carta1[0] and cartaBriscola[0] != carta2[0]: 
             # in caso di nessuna carta briscola da entrambe le parti vince il giocatore 1 che ha effettuato la prima giocata
             if carta1[0] != carta2[0]:
                 return 0
-            # in caso di carte diverse o entrambi briscole si contano i punti di validità della carta
+            # in caso di carte diverse si contano i punti di validità della carta
             elif carta1[0] == carta2[0]:
                 listaCarte = conteggioDeck[carta1[0]]
                 index_prima_carta = listaCarte.index(carta1[1])
@@ -24,7 +24,18 @@ class ValutazioneCarte:
                 if index_prima_carta < index_seconda_carta:
                     return 0
                 else:
-                    return 1
+                   return 1
+        
+        # condizione se tutti e due giocatori giocano una carta briscola
+        elif cartaBriscola[0] == carta2[0] and cartaBriscola[0] == carta1[0]:
+            listaCarte = conteggioDeck[carta1[0]]
+            index_prima_carta = listaCarte.index(carta1[1])
+            index_seconda_carta = listaCarte.index(carta2[1])
+            if index_prima_carta < index_seconda_carta:
+                return 0
+            else:
+                return 1
+
         # condizione se giocatore 1 gioca una carta briscola
         elif cartaBriscola[0] == carta1[0] and cartaBriscola[0] != carta2[0]:
             return 0
@@ -35,9 +46,9 @@ class ValutazioneCarte:
 
 # test
 if __name__ == "__main__":
-    x = [0, "5 di Coppe"]
-    y = [1, "Donna di Spade"]
-    z = [0, "2 di Coppe"]
+    x = [1, "7 di Spade"]
+    y = [2, "3 di Bastoni"]
+    z = [1, "2 di Spade"]
 
     value = ValutazioneCarte.valutaPresaBriscola(x, y, z)
     print(value)
