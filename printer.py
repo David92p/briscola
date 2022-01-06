8f6f3332df610bd77e3373c735221d791d4a7757
David92p <davide.panetta@hotmail.com>
2022-01-06 14:40:27 +0100

    fix branch

--- printer.py
+++ printer.py
@@ -2,31 +2,33 @@
 class Printer:
 
     def stampaCartegiocatore(carte):
-        oriz = '+'+'-'*25+'+'
+        oriz = '+'+'-'*20+'+'
         if len(carte) == 1:
             print(oriz)
-            print('+' + carte[0][1].upper().center(25, " ") + '+')
+            print('+' + carte[0][1].upper().center(20, " ") + '+')
             print(oriz)
         elif len(carte) == 2:
             print(oriz, end=" ")
             print(oriz)
-            print('+' + carte[0][1].upper().center(25, " ") + '+', end=" ")
-            print('+' + carte[1][1].upper().center(25, " ") + '+')
+            print('+' + carte[0][1].upper().center(20, " ") + '+', end=" ")
+            print('+' + carte[1][1].upper().center(20, " ") + '+')
             print(oriz, end=" ")
             print(oriz)
         elif len(carte) == 3:
             print(oriz, end=" ")
             print(oriz, end=" ")
             print(oriz)
-            print('+' + carte[0][1].upper().center(25, " ") + '+', end=" ")
-            print('+' + carte[1][1].upper().center(25, " ") + '+', end=" ")
-            print('+' + carte[2][1].upper().center(25, " ") + '+')
+            print('+' + carte[0][1].upper().center(20, " ") + '+', end=" ")
+            print('+' + carte[1][1].upper().center(20, " ") + '+', end=" ")
+            print('+' + carte[2][1].upper().center(20, " ") + '+')
             print(oriz, end=" ")
             print(oriz, end=" ")
             print(oriz)
 
     def stampaCarta(lista):
-        oriz = '+'+'-'*25+'+'
+        oriz = '+'+'-'*20+'+'
         print(oriz)
-        print('+' + lista[1].upper().center(25, " ") + '+')
-        print(oriz)
+        print('+' + lista[1].upper().center(20, " ") + '+')
+        print(oriz)      
+
+    
