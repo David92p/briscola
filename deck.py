8a71bc7cc49a4399d7a9cbc1a09382f0b4d8e4a1
David92p <davide.panetta@hotmail.com>
2022-01-06 15:19:51 +0100

    Merge remote-tracking branch 'origin' into briscola

---
+++ deck.py
@@ -0,0 +1,86 @@
+from random import shuffle, randint, sample
+
+#la seguente classe si occupa di gestire un deck di briscola
+class Deck:
+
+    #Di seguito un dizionario composto da chiave-seme e valore-carte Briscola
+    deckOrdinato = {"Coppe": ["Asso Coppe","2 di Coppe","3 di Coppe", "4 di Coppe", "5 di Coppe",
+                               "6 di Coppe", "7 di Coppe", "Donna di Coppe", "Cavallo di Coppe", "Re di Coppe"],
+                "Spade": ["Asso Spade","2 di Spade","3 di Spade", "4 di Spade", "5 di Spade",
+                          "6 di Spade", "7 di Spade", "Donna di Spade", "Cavallo di Spade", "Re di Spade"],
+                "Bastoni": ["Asso Bastoni","2 di Bastoni","3 di Bastoni", "4 di Bastoni", "5 di Bastoni",
+                            "6 di Bastoni", "7 di Bastoni", "Donna di Bastoni", "Cavallo di Bastoni", "Re di Bastoni"],
+                "Denari": ["Asso Denari","2 di Denari","3 di Denari", "4 di Denari", "5 di Denari",
+                        "6 di Denari", "7 di Denari", "Donna di Denari", "Cavallo di Denari", "Re di Denari"]
+            }
+
+    def __init__(self):
+        self.deckBriscola = Deck.mescolaDeck()
+
+
+    #Il metodo si occupa di restituire un dizionario del nostro deck in modalità disordinata
+    @staticmethod
+    def mescolaDeck():
+        deck = {}
+        for seme, carte in Deck.deckOrdinato.items():
+            shuffle(carte)
+            deck[seme] = carte
+        return deck
+
+    def definisciBriscola(self):
+        cartaBriscola = []
+        n = randint(0, 3)
+        cartaBriscola.append(n)
+        for i, seme in enumerate(self.deckBriscola):
+            if i == n:
+                for k, mazzo in self.deckBriscola.items():
+                    if seme == k:
+                        indiceCarta = randint(0, 9)
+                        cartaBriscola.append(mazzo[indiceCarta])
+                        del mazzo[indiceCarta]
+                        return cartaBriscola
+
+    # Il metodo si occupa di scegliere una carta random dal nostro deck e consegnarla ad un possibile giocatore
+    def consegnaCarta(self):
+         while True:
+            if len(self.deckBriscola["Coppe"]) == 0 and len(self.deckBriscola["Spade"]) == 0 and len(self.deckBriscola["Bastoni"]) == 0 and len(self.deckBriscola["Denari"]) == 0:
+                return ''
+            else:
+                sequenzaIndice = sample(range(4), 4)
+                carta = []
+                for el in sequenzaIndice: 
+                    for i, seme in enumerate(self.deckBriscola):
+                        if i == el:
+                            if len(self.deckBriscola[seme]) == 0:
+                                break
+                            else:
+                                for chiave, valori in self.deckBriscola.items():
+                                    if chiave == seme:
+                                        indiceCarta = randint(0, len(valori)-1)
+                                        carta.append(i)
+                                        carta.append(valori[indiceCarta])
+                                        del valori[indiceCarta]
+                                        return carta
+
+    
+
+    #Questo metodo è un controllo del nostro deck prima e dopo aver effettuato una giocata
+    def mostraDeck(self):
+        for carte in self.deckBriscola.values():
+            print(carte)
+
+
+# test
+if __name__ == "__main__":
+    deck = Deck()  # Creiamo un'istanza di tipo deck di briscola
+    # Utiliziamo il metodo 'mostraDeck' per stampare a video il deck
+    Deck.mostraDeck(deck)
+    print()
+    # Definiamo una briscola di gioco 
+    print(Deck.definisciBriscola(deck))
+    print()
+    # Consegnamo una carta dal deck
+    print(Deck.consegnaCarta(deck))
+    print()
+    # Mostriamo a video il deck utile come debugging
+    Deck.mostraDeck(deck)
---
+++ definisci_giocata.py
@@ -0,0 +1,111 @@
+
+class ValutazioneCarte:
+
+    def valutaPresaBriscola(cartaBriscola, carta1, carta2):
+        conteggioDeck = [["Asso Coppe", "3 di Coppe", "Re di Coppe", "Cavallo di Coppe", "Donna di Coppe", "7 di Coppe",
+                          "6 di Coppe", "5 di Coppe", "4 di Coppe", "2 di Coppe"],
+                         ["Asso Spade", "3 di Spade", "Re di Spade", "Cavallo di Spade", "Donna di Spade", "7 di Spade",
+                          "6 di Spade", "5 di Spade", "4 di Spade", "2 di Spade"],
+                         ["Asso Bastoni", "3 di Bastoni", "Re di Bastoni", "Cavallo di Bastoni", "Donna di Bastoni", "7 di Bastoni",
+                          "6 di Bastoni", "5 di Bastoni", "4 di Bastoni", "2 di Bastoni"],
+                         ["Asso Denari", "3 di Denari", "Re di Denari", "Cavallo di Denari", "Donna di Denari", "7 di Denari",
+                          "6 di Denari", "5 di Denari", "4 di Denari", "2 di Denari"]]
+
+        # condizione se nessuno dei due giocatori giocano una carta briscola
+        if cartaBriscola[0] != carta1[0] and cartaBriscola[0] != carta2[0]:
+            # condizione di nessuna carta di briscola da entrambe le parti e diverso seme di giocata
+            if carta1[0] != carta2[0]:
+                return 0
+            # condizione di nessuna carta briscola da entrambe le parti e stesso seme di giocata
+            elif carta1[0] == carta2[0]:
+                listaCarte = conteggioDeck[carta1[0]]
+                index_prima_carta = listaCarte.index(carta1[1])
+                index_seconda_carta = listaCarte.index(carta2[1])
+                if index_prima_carta < index_seconda_carta:
+                    return 0
+                else:
+                    return 1
+
+        # condizione se tutti e due giocatori giocano una carta briscola
+        elif cartaBriscola[0] == carta2[0] and cartaBriscola[0] == carta1[0]:
+            listaCarte = conteggioDeck[carta1[0]]
+            index_prima_carta = listaCarte.index(carta1[1])
+            index_seconda_carta = listaCarte.index(carta2[1])
+            if index_prima_carta < index_seconda_carta:
+                return 0
+            else:
+                return 1
+
+        # condizione se giocatore 1 gioca una carta briscola
+        elif cartaBriscola[0] == carta1[0] and cartaBriscola[0] != carta2[0]:
+            return 0
+        # condizione se giocatore 2 gioca una carta briscola
+        elif cartaBriscola[0] != carta1[0] and cartaBriscola[0] == carta2[0]:
+            return 1
+
+    # La seguente funzione prende in considerazione solo le carte che determinano dei punti vincenti
+    # Per ogni carta definita come carta-punti, viene aggiunto il valore dei punti al giocatore che ha la carta tra le proprie prese
+    def conteggioPuntiBriscola(listaCarteGiocatori):
+        puntiGiocatore1 = 0
+        puntiGiocatore2 = 0
+        puntiAsso = ["Asso Coppe", "Asso Spade", "Asso Bastoni", "Asso Denari"]
+        punti_3 = ["3 di Coppe", "3 di Spade", "3 di Bastoni", "3 di Denari", ]
+        puntiRe = ["Re di Coppe", "Re di Spade",
+                   "Re di Bastoni", "Re di Denari"]
+        puntiCavallo = ["Cavallo di Coppe", "Cavallo di Spade",
+                        "Cavallo di Bastoni", "Cavallo di Denari"]
+        puntiDonna = ["Donna di Coppe", "Donna di Spade",
+                      "Donna di Bastoni", "Donna di Denari"]
+
+        # ciclo di controllo carte 'Asso' all'interno delle carte punti prese dai giocatori
+        # la carta attribuisce 11 punti al giocatore che la detiene
+        for carta in puntiAsso:
+            if carta in listaCarteGiocatori[0]:
+                puntiGiocatore1 += 11
+            else:
+                puntiGiocatore2 += 11
+
+        # ciclo di controllo carte '3' all'interno delle carte punti prese dai giocatori
+        # la carta attribuisce 10 punti al giocatore che la detiene
+        for carta in punti_3:
+            if carta in listaCarteGiocatori[0]:
+                puntiGiocatore1 += 10
+            else:
+                puntiGiocatore2 += 10
+
+        # ciclo di controllo carte 'Re' all'interno delle carte punti prese dai giocatori
+        # la carta attribuisce 4 punti al giocatore che la detiene
+        for carta in puntiRe:
+            if carta in listaCarteGiocatori[0]:
+                puntiGiocatore1 += 4
+            else:
+                puntiGiocatore2 += 4
+
+        # ciclo di controllo carte 'Cavallo' all'interno delle carte punti prese dai giocatori
+        # la carta attribuisce 3 punti al giocatore che la detiene
+        for carta in puntiCavallo:
+            if carta in listaCarteGiocatori[0]:
+                puntiGiocatore1 += 3
+            else:
+                puntiGiocatore2 += 3
+
+        # ciclo di controllo carte 'Donna' all'interno delle carte punti prese dai giocatori
+        # la carta attribuisce 2 punti al giocatore che la detiene
+        for carta in puntiDonna:
+            if carta in listaCarteGiocatori[0]:
+                puntiGiocatore1 += 2
+            else:
+                puntiGiocatore2 += 2
+
+        # abbiamo una lista di return con indice 0 carte punti primo giocatore e indice 1 carte punti secondo giocatore
+        return [puntiGiocatore1, puntiGiocatore2]
+
+
+# test
+if __name__ == "__main__":
+    x = [1, "7 di Spade"]
+    y = [2, "3 di Bastoni"]
+    z = [1, "2 di Spade"]
+
+    value = ValutazioneCarte.valutaPresaBriscola(x, y, z)
+    print(value)
